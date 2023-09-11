e8667ff45ac7b25f35f42f731813c1e2dae1c33d
isabelle913 <atelierisabelle@videotron.ca>
2023-09-11 19:31:18 -0400

    Ajout bouton install PWA

---
+++ script/install.js
@@ -0,0 +1,37 @@
+let deferredInstallPrompt = null;
+const installBtn = document.getElementById("installBtn");
+
+// console.log("installBtn", installBtn);
+
+installBtn.addEventListener("click", installPWA);
+window.addEventListener("beforeInstallPrompt", saveBeforeInstallPromptEvent);
+window.addEventListener("appinstalled", logAppInstalled);
+
+function installPWA(event) {
+  console.log(event);
+  console.log("Miip");
+
+  deferredInstallPrompt.prompt();
+  event.srcElement.setAttribute("hidden", true);
+
+  deferredInstallPrompt.userChoice.then((choice) => {
+    if (choice.outcome === "accepted") {
+      console.log("User accepted", choice);
+    } else {
+      console.log("User refused", choice);
+    }
+    deferredInstallPrompt = null;
+  });
+}
+
+function saveBeforeInstallPromptEvent(event) {
+  console.log("saveBeforeInstallPromptEvent", event);
+
+  deferredInstallPrompt = event;
+
+  installBtn.removeAttribute("hidden");
+}
+
+function logAppInstalled(event) {
+  console.log("App déja installé", event);
+}
