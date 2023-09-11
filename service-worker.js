6ab317ad98d605a550d66c4f8314f208eb8e39c3
isabelle913 <atelierisabelle@videotron.ca>
2023-09-11 19:32:21 -0400

    changé version static-cache-V4

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // console.log("fichier service-worker");
 
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v3";
+const CACHE_NAME = "static-cache-v4";
 //Add list of files to cache here.
 const FILES_TO_CACHE = ["index.html", "ou-aller.html", "quoi-faire.html"];
 
